1d04a3223c721daaf8cf96d20f24b8c7c004b70a
Alex Wolinsky <alexwolinsky1@gmail.com>
2020-12-29 20:15:13 -0800

    __tests__ directory

---
+++ __tests__/client/components/AccountInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+// import necessary components here
+
+configure({ adapter: new Adapter() });
+
+describe('AccountInfo React unit tests', () => {
+
+});
